11c2f3d7dac757c42c4754a42cc67259e26f906b
Dahuiss <76972002+Dahuiss@users.noreply.github.com>
2023-05-23 16:01:41 +0200

    Test

--- src/test/java/singleton/BatSingletonStepDefs.java
+++ src/test/java/singleton/BatSingletonStepDefs.java
@@ -1,5 +1,68 @@
 package singleton;
 
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import superhero.SuperHero;
+import car.voiture.Voiture;
+import org.junit.Assert;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 public class BatSingletonStepDefs {
 
+    private BatSingleton batSingleton;
+    private SuperHero superHero;
+    private Voiture voiture;
+@Given("I instantiate the Batman singleton")
+public void i_have_accessed_the_batman_singleton() {
+    batSingleton = BatSingleton.getInstance();
+    throw new io.cucumber.java.PendingException();
+}
+@When("I request the instantiated SuperHero")
+public void i_request_the_instantiated_super_hero() {
+    superHero = batSingleton.getSuperHero();
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+@Then("the SuperHero should have the name {string}")
+public void the_super_hero_should_have_the_name(String string) {
+    Assert.assertEquals(string, superHero.getNom());
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+@Then("the SuperHero should have the power {string}")
+public void the_super_hero_should_have_the_power(String string) {
+    Assert.assertEquals(string, superHero.getPouvoir());
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+@Then("the SuperHero should have the level {int}")
+public void the_super_hero_should_have_the_level(Integer int1) {
+    Assert.assertEquals(Optional.ofNullable(int1), Optional.of(superHero.getNiveau()));
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+@When("I request the instantiated Voiture")
+public void i_request_the_instantiated_voiture() {
+    voiture = batSingleton.getVoiture();
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+@Then("the Voiture should have the owner {string}")
+public void the_voiture_should_have_the_owner(String string) {
+    Assert.assertEquals(string, voiture.getOwner());
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+@Then("the Voiture should have the year {int}")
+public void the_voiture_should_have_the_year(Integer int1) {
+    Assert.assertEquals(int1, Optional.of(Optional.of(voiture.getAnnee())));
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
 }
--- src/test/resources/batsingleton.feature
+++ src/test/resources/batsingleton.feature
@@ -1,7 +1,15 @@
-Feature: US_OO3 - Vérifier qu'on instancie correctement
+Feature: Batman Singleton
 
-  Scenario: Verifier les details du superhero Batman
-    Given un objet de type SuperHero
-    When je demande les informations du superhero Batman
-    Then Je dois avoir le nom "<nom>" et le pouvoir "<pouvoir>" et le niveau "<niveau>"
+  Scenario Outline: Verify instantiated SuperHero and Voiture
+    Given I instantiate a BatSingeton
+    When I request the instantiated SuperHero
+    Then the SuperHero should have the name "<name>"
+    And the SuperHero should have the power "<power>"
+    And the SuperHero should have the level <level>
+    When I request the instantiated Voiture
+    Then the Voiture should have the owner "<owner>"
+    And the Voiture should have the year <year>
 
+  Examples:
+    | name   | power   | level  | owner     | year |
+    | Batman | Voler   | 10000  | Batmobile | 2022 |
